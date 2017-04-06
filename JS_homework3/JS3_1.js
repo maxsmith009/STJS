4fffc7f199b7f070ca6f7a73249e07a0bb2a64ed
maxsmith009 <max03246@gmail.com>
2017-04-06 23:52:06 +0300

    JS3

---
+++ JS_homework3/JS3_1.js
@@ -0,0 +1,67 @@
+var obj = [2,'sf',{a:9},{},64,235];
+    for (var i=0;i<obj.length;i++) {
+        var sObj = JSON.stringify(obj[i]);
+        localStorage.setItem(i, sObj);
+    }
+
+
+    var mod = (function Module(){
+
+        
+
+        function getKey(key){
+
+            return JSON.parse(localStorage.getItem(key));
+
+        }
+
+        function getAll(){
+            
+            return localStorage;
+
+
+        }
+
+        function set(key,value){
+
+            return localStorage.setItem(key,JSON.stringify(value))
+
+        }
+
+        function remove(key){
+
+            return localStorage.removeItem(key);
+
+        }
+
+        function clear(){
+
+            return localStorage.clear();
+
+        }
+
+        function add(arr){
+
+            for (var i=0;i<arr.length;i++) {
+                for (var key in arr[i]) {
+                    var value = JSON.stringify(arr[i][key]);
+                    localStorage.setItem(key, value);
+                }
+            }
+        }
+
+        return {
+
+            getKey:getKey,
+            getAll:getAll,
+            set:set,
+            remove:remove,
+            clear:clear,
+            add:add
+
+        }
+
+    })();
+    
+    mod.add([{'a':7}]);
+    mod.getAll();
---
+++ JS_homework3/README.md
@@ -0,0 +1,3 @@
+Третья домашка
+
+Написать модуль, который работает с LocalStorage. Он должен возвращать объект со св-вами: getkey -> значение из LS, getall() -> массив объектов [{key: val}, ...], set(key, value), remove(key), clear(), add([{key: val}, ...]). Продемонстрировать работу модуля. Использовать JSON.parse | JSON.stringify.
